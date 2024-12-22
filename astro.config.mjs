cc79da871737f4bb43a56a953908414248eeef40
Sammie Walker <97995752+xwalksd@users.noreply.github.com>
2024-12-21 19:26:09 -0600

    Updated some content

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astrofy-template.netlify.app',
-  integrations: [mdx(), sitemap(), tailwind()]
+  site: "https://xwalksd.github.io",
+  base: "/xwalksd.github.io",
+  integrations: [mdx(), sitemap(), tailwind()],
 });
