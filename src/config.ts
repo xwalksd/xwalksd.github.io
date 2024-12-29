4f9b3219311a7d28adb21f6d159a5d302d88deb9
Sammie Walker <97995752+xwalksd@users.noreply.github.com>
2024-12-28 21:06:23 -0600

    Removed and updated meta information

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Hi, I\'m Sammie Walker';
-export const SITE_DESCRIPTION = 'Hello, this is my portfolio/cv website. I\'m so glad you came!';
-export const GENERATE_SLUG_FROM_TITLE = true
-export const TRANSITION_API = true
+export const SITE_TITLE = "Sammie Walker: SWE";
+export const SITE_DESCRIPTION =
+  "Hello, this is my portfolio/cv website. I'm so glad you came!";
+export const GENERATE_SLUG_FROM_TITLE = true;
+export const TRANSITION_API = true;
